801cc29155d8588512487e713a9dffb602fce411
samuel-rosa <alessandrosamuelrosa@gmail.com>
2025-07-03 20:45:42 -0300

    Refactor validation statistics calculations: enhance error statistics for observed and predicted values across multiple regions and groups

--- src/46_soc_models_validation.R
+++ src/46_soc_models_validation.R
@@ -17,29 +17,84 @@ res_tab_path <- "res/tab/"
 random_seed <- 1984
 
 # Read validation data
-# "res/tab/soc_model01_predictions.txt", sep = "\t"
-# "res/tab/soc_model02_predictions.txt", sep = "\t"
-# "res/tab/soc_model03_predictions.txt", sep = "\t"
 soc_model01 <- data.table::fread("res/tab/soc_model01_predictions.txt", sep = "\t", header = TRUE)
 soc_model02 <- data.table::fread("res/tab/soc_model02_predictions.txt", sep = "\t", header = TRUE)
 soc_model03 <- data.table::fread("res/tab/soc_model03_predictions.txt", sep = "\t", header = TRUE)
-soc_model <- rbind(soc_model01, soc_model02, soc_model03)
+soc_model <- rbind(soc_model01, soc_model02, soc_model03, fill = TRUE)
+soc_model[, estoque := estoque / 100] # Convert to t/ha
+soc_model[, predicted := predicted / 100] # Convert to t/ha
 dim(soc_model)
-# 15659     4
+# 15659     10
 
-# General validation statistics (t/ha)
-error_statistics(observed = soc_model$estoque / 100, predicted = soc_model$predicted / 100)
-
-# Average observed and predicted values over groups
 # Create groups
 soc_model[, group_id := sub("-REP.*", "", dataset_id)]
-# Calculate average observed and predicted values
+# Calculate average over groups
 soc_model_avg <- soc_model[, .(
-  avg_observed = mean(estoque, na.rm = TRUE),
-  avg_predicted = mean(predicted, na.rm = TRUE)
-), by = group_id]
-# Calculate error statistics for average values (t/ha)
-error_statistics(
-  observed = soc_model_avg$avg_observed / 100,
-  predicted = soc_model_avg$avg_predicted / 100
+  estoque = mean(estoque, na.rm = TRUE),
+  predicted = mean(predicted, na.rm = TRUE),
+  Amazonia = max(Amazonia, na.rm = TRUE),
+  Pampa = max(Pampa, na.rm = TRUE),
+  Cerrado = max(Cerrado, na.rm = TRUE),
+  Pantanal = max(Pantanal, na.rm = TRUE),
+  Caatinga = max(Caatinga, na.rm = TRUE),
+  Mata_Atlantica = max(Mata_Atlantica, na.rm = TRUE)
+  ),
+  by = group_id]
+dim(soc_model_avg)
+# 12196     3
+
+# Validation statistics - Brazil ###################################################################
+rbind(
+  # General validation statistics (t/ha)
+  soc_model[, error_statistics(observed = estoque, predicted = predicted)],
+  # Calculate error statistics for average values (t/ha)
+  soc_model_avg[, error_statistics(observed = estoque, predicted = predicted)]
+)
+
+# Validation statistics - Amazon ###################################################################
+rbind(
+  # General validation statistics (t/ha)
+  soc_model[Amazonia == 1, error_statistics(observed = estoque, predicted = predicted)],
+  # Calculate error statistics for average values (t/ha)
+  soc_model_avg[Amazonia == 1, error_statistics(observed = estoque, predicted = predicted)]
+)
+
+# Validation statistics - Pampa ###################################################################
+rbind(
+  # General validation statistics (t/ha)
+  soc_model[Pampa == 1, error_statistics(observed = estoque, predicted = predicted)],
+  # Calculate error statistics for average values (t/ha)
+  soc_model_avg[Pampa == 1, error_statistics(observed = estoque, predicted = predicted)]
+)
+
+# Validation statistics - Cerrado ###################################################################
+rbind(
+  # General validation statistics (t/ha)
+  soc_model[Cerrado == 1, error_statistics(observed = estoque, predicted = predicted)],
+  # Calculate error statistics for average values (t/ha)
+  soc_model_avg[Cerrado == 1, error_statistics(observed = estoque, predicted = predicted)]
+)
+
+# Validation statistics - Pantanal #################################################################
+rbind(
+  # General validation statistics (t/ha)
+  soc_model[Pantanal == 1, error_statistics(observed = estoque, predicted = predicted)],
+  # Calculate error statistics for average values (t/ha)
+  soc_model_avg[Pantanal == 1, error_statistics(observed = estoque, predicted = predicted)]
+)
+
+# Validation statistics - Caatinga #################################################################
+rbind(
+  # General validation statistics (t/ha)
+  soc_model[Caatinga == 1, error_statistics(observed = estoque, predicted = predicted)],
+  # Calculate error statistics for average values (t/ha)
+  soc_model_avg[Caatinga == 1, error_statistics(observed = estoque, predicted = predicted)]
+)
+
+# Validation statistics - Mata Atlantica ###########################################################
+rbind(
+  # General validation statistics (t/ha)
+  soc_model[Mata_Atlantica == 1, error_statistics(observed = estoque, predicted = predicted)],
+  # Calculate error statistics for average values (t/ha)
+  soc_model_avg[Mata_Atlantica == 1, error_statistics(observed = estoque, predicted = predicted)]
 )
