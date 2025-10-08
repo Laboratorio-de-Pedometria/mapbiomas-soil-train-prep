25bdb8d47f7f55609db3b87c04affe23055a2df6
samuel-rosa <alessandrosamuelrosa@gmail.com>
2025-10-08 01:03:40 -0300

    Refactor data merging process in 10_merge_curated_data.R: streamline curated data reading and update summary statistics

--- src/10_merge_curated_data.R
+++ src/10_merge_curated_data.R
@@ -22,36 +22,22 @@ curated_files <- list.files(
   pattern = "^ctb[0-9]{4}\\.csv$",
   full.names = TRUE, recursive = TRUE
 )
-length(curated_files) # 28 datasets
+length(curated_files) # 37 datasets
 print(curated_files)
-
-# Set of columns to read from curated data
-read_cols <- c(
-  "dataset_id", "observacao_id",
-  "data_ano", "ano_fonte",
-  "coord_x", "coord_y", "coord_precisao", "coord_fonte", "coord_datum",
-  "pais_id", "estado_id", "municipio_id",
-  "amostra_area",
-  "taxon_sibcs",
-  "camada_nome", "camada_id", "amostra_id",
-  "profund_sup", "profund_inf",
-  "carbono", "ctc", "ph", "argila", "silte", "areia", "terrafina", "dsi"
-)
 
 # If length(curated_files) is larger than 0, read all files and store them in a list
 if (length(curated_files) > 0) {
   curated_list <- lapply(curated_files, data.table::fread)
-  # rbind all datasets keeping only the matching columns
   curated_data <- data.table::rbindlist(curated_list, fill = TRUE)
-  curated_data <- curated_data[, ..read_cols]
   curated_data[, id := paste0(dataset_id, "-", observacao_id)]
 } else {
   stop("No curated files found")
 }
 summary_soildata(curated_data)
-# Layers:
-# Events:
-# Georeferenced events:
+# Layers: 12937
+# Events: 5445
+# Georeferenced events: 4992
+# Datasets: 37
 
 # Read the latest Brazilian Soil Dataset ###########################################################
 # Check if "data/00_brazilian_soil_dataset_2024.txt" exists. If not, read the Brazilian
@@ -75,31 +61,31 @@ summary_soildata(br_soil2024)
 # Georeferenced events: 14334
 # Datasets: 255
 
-# # Merge curated data with SoilData
-# data.table::setnames(soildata, old = "data_coleta_ano", new = "data_ano")
-# data.table::setnames(soildata, old = "coord_datum_epsg", new = "coord_datum")
-# curated_ctb <- curated_data[, unique(dataset_id)]
-# soildata <- soildata[!dataset_id %in% curated_ctb]
-# summary_soildata(soildata)
-# # Layers: 51044
-# # Events: 14761
-# # Georeferenced events: 11633
-# soildata <- rbind(soildata, curated_data, fill = TRUE)
-# summary_soildata(soildata)
-# # Layers: 60862
-# # Events: 18415
-# # Georeferenced events: 14990
+# Merge curated data with SoilData
+curated_ctb <- curated_data[, unique(dataset_id)]
+br_soil2024 <- br_soil2024[!dataset_id %in% curated_ctb]
+summary_soildata(br_soil2024)
+# Layers: 47792
+# Events: 13566
+# Georeferenced events: 11117
+# Datasets: 226
+soildata <- rbind(br_soil2024, curated_data, fill = TRUE)
+summary_soildata(soildata)
+# Layers: 60729
+# Events: 5446
+# Georeferenced events: 4993
+# Datasets: 263
 
 # Check spatial distribution
 if (FALSE) {
   x11()
-  soildata_sf <- br_soil2024[!is.na(coord_x) & !is.na(coord_y)]
+  soildata_sf <- soildata[!is.na(coord_x) & !is.na(coord_y)]
   soildata_sf <- sf::st_as_sf(soildata_sf, coords = c("coord_x", "coord_y"), crs = 4326)
   plot(soildata_sf["estado_id"], cex = 0.5)
 }
 
-br_soil2024[, id := paste0(dataset_id, "-", observacao_id)]
+soildata[, id := paste0(dataset_id, "-", observacao_id)]
 
 # Export cleaned data ##############################################################################
-summary_soildata(br_soil2024)
-data.table::fwrite(br_soil2024, "data/10_soildata.txt", sep = "\t")
+summary_soildata(soildata)
+data.table::fwrite(soildata, "data/10_soildata.txt", sep = "\t")
