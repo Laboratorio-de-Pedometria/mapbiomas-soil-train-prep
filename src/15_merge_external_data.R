0cd0150efa8ff4fd7c688881fe58b16711011112
samuel-rosa <alessandrosamuelrosa@gmail.com>
2025-10-23 13:53:09 -0300

    Add functionality to read and merge external SHP files for pseudo-samples

--- src/15_merge_external_data.R
+++ src/15_merge_external_data.R
@@ -18,3 +18,41 @@ if (!requireNamespace("sf")) {
 # Source helper functions
 source("src/00_helper_functions.r")
 
+# SOILDATA
+
+# Read data from previous processing script
+file_path <- "data/12_soildata.txt"
+soildata <- data.table::fread(file_path, sep = "\t", na.strings = c("", "NA", "NaN"))
+summary_soildata(soildata)
+# Layers: 51152
+# Events: 16994
+# Georeferenced events: 14372
+# Datasets: 259
+
+
+# EXTERNAL DATA ####################################################################################
+
+# Pseudo-samples: dunes, beaches
+folder_path <- "data/2025_10_23_pseudo_amostras_dunas_praias_areiais"
+# List all SHP files in the folder
+shp_files <- list.files(
+  path = folder_path,
+  pattern = "\\.shp$",
+  full.names = TRUE, recursive = TRUE
+)
+# Read and merge all SHP files
+pseudo_list <- lapply(shp_files, sf::st_read)
+pseudo_data <- do.call(rbind, pseudo_list)
+
+if (FALSE) {
+  mapview::mapview(pseudo_data)
+}
+
+# Extract the coordinates into a data.table
+pseudo_dt <- data.table::as.data.table(sf::st_coordinates(pseudo_data))
+
+# Set columns
+# dataset_id: ctbsand
+# obsert
+
+
